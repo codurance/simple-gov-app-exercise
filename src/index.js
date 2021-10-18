cb2db9baf246ab1bada59482293dfbdc3439b0c9
Niall <niall.bambury@codurance.com>
2021-10-18 14:15:49 +0100

    [NB] Add fathers details page

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Homepage } from './pages/homepage/Homepage';
 import { MyDetailsRoutes } from './pages/myDetails/MyDetailsRoutes';
+import { FathersDetailsRoutes } from './pages/fathersDetails/FathersDetailsRoutes';
 import './index.css'
 import {
   BrowserRouter as Router,
@@ -13,6 +14,7 @@ ReactDOM.render(
   <React.StrictMode>
     <Router>
       <MyDetailsRoutes />
+      <FathersDetailsRoutes />
       <Switch>
         <Route exact path="/">
           <Homepage />
--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -13,11 +13,15 @@ function Homepage() {
                   <InsetText>Processing of your details can take upto 8 weeks</InsetText>
                   <p>Once you've completed each section below you will be given the chance to review your responses before submitting them</p>
                   <p>You will be asked basic information about yourself and your parents. You can review and amend your answers before submitting them.</p>
-                    <div>
-                      <h3>Your details:</h3>
-                      <Link data-testid="my-details-link" to="/my-details">Your details page</Link>
-                    </div>
+                  <div>
+                    <h3>Your details:</h3>
+                    <Link data-testid="my-details-link" to="/my-details">Your details page</Link>
                   </div>
+                  <div>
+                    <h3>Your fathers details:</h3>
+                    <Link data-testid="fathers-details-link" to="/fathers-details">Your fathers details page</Link>
+                  </div>
+                </div>
             </Page>
           <Footer />
       </>
